3efe7953bad0b68d4d8762cbb5512da0bff0826d
michelle <113373744+mfarrell23@users.noreply.github.com>
2022-10-30 22:07:28 -0600

    changing port to env variable

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const path = require('path');
 const fs= require('fs');
 const notes = require('./db/db.json');
 const uuid= require('uuid');
-const PORT = 3001;
+
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,10 +13,13 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
+
 );
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+
 
 // GET request for notes
 app.get('/api/notes', (req, res) => {
@@ -50,12 +52,13 @@ app.post('/api/notes', (req, res) => {
         console.log(err)
     }else{
         console.log('new note added')
+        console.log(`PORT is: ${process.env.PORT}`);
     }
     })
 });
 
 
   
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+app.listen(process.env.PORT, () =>
+  console.log(`App listening at http://localhost:${process.env.PORT} 🚀`)
     );
